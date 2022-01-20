978eb0793d249ae737678d08c7199a0801138868
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-01-19 23:48:42 -0600

    PostConstruct did not work created constructor called method

--- src/main/java/com/springdemo/rest/StudentRestController.java
+++ src/main/java/com/springdemo/rest/StudentRestController.java
@@ -3,8 +3,6 @@ package com.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +17,7 @@ public class StudentRestController {
 
 	// define @PostConstructor to load the student data .. only once
 
-	@PostConstruct
+//	@PostConstruct  // Did not work, below called constructor instead
 	public void loadData() {
 		theStudents = new ArrayList<>();
 
@@ -28,16 +26,14 @@ public class StudentRestController {
 		theStudents.add(new Student("Mary", "Smith"));
 	}
 
-	// define end-point for "/students" - return list of students
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 
-//		List<Student> theStudents = new ArrayList<>();
-//		theStudents.add(new Student("Poornima", "Patel"));
-//		theStudents.add(new Student("Mario", "Rossi"));
-//		theStudents.add(new Student("Mary", "Smith"));
-
 		return theStudents;
+	}
+	
+	public StudentRestController() {
+		loadData();
 	}
 
 }
